a95bb07055b34905e05b1a642bf882dddfcaf121
luvuyo-cyber <136644800+luvuyo-cyber@users.noreply.github.com>
2025-01-08 22:29:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ import dotenv from "dotenv";
 
 async function main() {
   dotenv.config(); //load .env variables
-  const client = new mongodb.MongoClient(process.env.MOVIEREVIEWS_DB_URI);
+  const client = new mongodb.MongoClient(process.env.MOVIEREVIEWS_DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
   const port = process.env.PORT || 8000; //retrieve port & alt
   try {
     // Connect to the MongoDB cluster
